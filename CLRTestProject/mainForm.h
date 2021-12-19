7d424b5d764fcb7ac2eaa64f7a09421125914e2e
нет2 каби <a_sklemin@mail.ru>
2021-12-19 14:17:12 +0400

    pictrures container as button

--- CLRTestProject/mainForm.h
+++ CLRTestProject/mainForm.h
@@ -34,6 +34,9 @@ namespace CLRTestProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::PictureBox^ firstButton;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,14 +51,39 @@ namespace CLRTestProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(mainForm::typeid));
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->firstButton = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->firstButton))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->panel1->Location = System::Drawing::Point(191, 72);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(860, 423);
+			this->panel1->TabIndex = 0;
+			// 
+			// firstButton
+			// 
+			this->firstButton->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"firstButton.Image")));
+			this->firstButton->Location = System::Drawing::Point(52, 144);
+			this->firstButton->Name = L"firstButton";
+			this->firstButton->Size = System::Drawing::Size(100, 50);
+			this->firstButton->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->firstButton->TabIndex = 1;
+			this->firstButton->TabStop = false;
+			this->firstButton->Click += gcnew System::EventHandler(this, &mainForm::firstButton_Click);
 			// 
 			// mainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->BackColor = System::Drawing::SystemColors::ControlDarkDark;
 			this->ClientSize = System::Drawing::Size(1050, 492);
+			this->Controls->Add(this->firstButton);
+			this->Controls->Add(this->panel1);
 			this->ForeColor = System::Drawing::SystemColors::ButtonFace;
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->MaximizeBox = false;
@@ -63,15 +91,19 @@ namespace CLRTestProject {
 			this->Name = L"mainForm";
 			this->SizeGripStyle = System::Windows::Forms::SizeGripStyle::Show;
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Text = L"mainForm";
+			this->Text = L"Клиническая больница № 6";
 			this->TransparencyKey = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(224)), static_cast<System::Int32>(static_cast<System::Byte>(224)),
 				static_cast<System::Int32>(static_cast<System::Byte>(224)));
 			this->Load += gcnew System::EventHandler(this, &mainForm::mainForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->firstButton))->EndInit();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void mainForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void firstButton_Click(System::Object^ sender, System::EventArgs^ e) {
+		MessageBox::Show("Вы ввели неверный пароль", "Сообщение", MessageBoxButtons::OK, MessageBoxIcon::Warning);
 	}
 	};
 }
